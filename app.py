bde22a5e6056af8c8133550836f1acb788c0d019
ksuraj <keerthissec@gmail.com>
2022-06-23 07:39:57 +0530

    adding summarizer model

--- app.py
+++ app.py
@@ -1,43 +1,18 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import numpy as np
-from PIL import Image
-import keras
-from huggingface_hub import from_pretrained_keras
-# from io import BytesIO
-import imageio
-import pathlib
+from transformers import pipeline
 
-model = from_pretrained_keras("keras-io/lowlight-enhance-mirnet", compile=False)
+def run_model(text):
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    output = summarizer(text, max_length=130, min_length=30, do_sample=False)
+    return output[0]['summary_text']
 
-def run_model(image):
-    
-    output_path = os.path.join('static/images')
-    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True) 
-    image = Image.open(image)
-    image = image.resize((960,640))
-    image = keras.preprocessing.image.img_to_array(image)
-    image = image.astype("float32") / 255.0
-    image = np.expand_dims(image, axis=0)
-    output = model.predict(image)
-    output_image = output[0] * 255.0
-    output_image = output_image.clip(0, 255)
-    output_image = output_image.reshape(
-        (np.shape(output_image)[0], np.shape(output_image)[1], 3)
-    )
-    output_image = np.uint32(output_image)
-    output_image = output_image.astype(np.uint8)
-    output_file = os.path.join(output_path, 'output.jpeg')
-    imageio.imwrite(output_file, output_image)
-    return output_file
-
-def main(input_image):
+def main(input_text):
     """
     Argument(s) name in the function signature are matching with the id in the
     app.config
     """
-    output = run_model(input_image)
+    output = run_model(input_text)
+    # print(output)
     return output
 
-if __name__=="__main__":
-    main('download.png')
+# if __name__=="__main__":
+#     main('The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.')
